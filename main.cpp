172187d096b871cc0c937630664db645a2db8b8d
Tom Do <tom@iifuture.com>
2022-07-10 13:45:35 +1000

    Added white pieces

--- main.cpp
+++ main.cpp
@@ -26,6 +26,16 @@ int main(int argc, char const *argv[])
     Piece bp7({6,2},1,'P');
     Piece bp8({7,2},1,'P');
 
+
+    Piece wp1({0,6},0,'P');
+    Piece wp2({1,6},0,'P');
+    Piece wp3({2,6},0,'P');
+    Piece wp4({3,6},0,'P');
+    Piece wp5({4,6},0,'P');
+    Piece wp6({5,6},0,'P');
+    Piece wp7({6,6},0,'P');
+    Piece wp8({7,6},0,'P');
+
     std::cout << bp1.boardpos[1] << std::endl;
     return 0;
 }
